3983f42845071f915e58620f44835e306d68a163
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-06-10 20:58:02 +0300

    init

---
+++ textTags/js/app.js
@@ -0,0 +1,6 @@
+// const currentTime = document.querySelector("date");
+
+// setInterval(function() {
+//   currentTime.textContent = new Date().toLocaleDateString();
+//   currentTime.textContent += "    " + new Date().toLocaleTimeString();
+// }, 16);
